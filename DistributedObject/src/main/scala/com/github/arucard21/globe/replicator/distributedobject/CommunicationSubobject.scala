4558c9d2f45492d26070ad14a08beadd9a6a5ba6
arucard21 <arucard21@gmail.com>
2020-03-11 13:11:10 +0100

    Add placeholder methods to communications subobject

--- DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/CommunicationSubobject.scala
+++ DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/CommunicationSubobject.scala
@@ -2,4 +2,20 @@ package com.github.arucard21.globe.replicator.distributedobject
 
 object CommunicationSubobject {
 
+  def send_request = {
+    ???
+  }
+
+  def register = {
+    ???
+  }
+
+  def acquire_lock = {
+    ???
+  }
+
+  def release_lock = {
+    ???
+  }
+
 }
