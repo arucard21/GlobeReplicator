2dbe08d91977326b269032db6671f4e88800e7db
arucard21 <arucard21@gmail.com>
2020-03-11 17:13:05 +0100

    Register the distributed object with the Lookup Service on startup

--- DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/CommunicationSubobject.scala
+++ DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/CommunicationSubobject.scala
@@ -1,13 +1,52 @@
 package com.github.arucard21.globe.replicator.distributedobject
 
+import java.net.URI
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse, StatusCodes, Uri}
+import akka.stream.ActorMaterializer
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+case class Registration(name: String, location: URI)
+
 object CommunicationSubobject {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
 
   def send_request = {
     ???
   }
 
-  def register = {
-    ???
+  def register(lookupServiceUri : URI, name : String, location : URI): Boolean = {
+    val registration = new Registration(name, location)
+    val registrationJson : String = mapper.writeValueAsString(registration)
+    val registrationUri = Uri(lookupServiceUri.toString).withPath(Path("/register"))
+
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+      method = HttpMethods.POST,
+      uri = registrationUri,
+      entity = registrationJson
+    ))
+    Await.result(
+      responseFuture.map { response =>
+        if (response.status == StatusCodes.OK){
+          true
+        }
+        else{
+          false
+        }
+      },
+      Duration.Inf)
   }
 
   def acquire_lock = {
--- DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/DOApplication.scala
+++ DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/DOApplication.scala
@@ -1,5 +1,24 @@
 package com.github.arucard21.globe.replicator.distributedobject
 
+import java.net.URI
+
+import scala.sys.SystemProperties
+
 object DOApplication extends App {
-  DOServer.startServer("0.0.0.0", 8080)
+  val PropertyKeyLookupServiceUrl : String = "lookupservice.url"
+  val PropertyKeyDistributedObjectUrl : String = "distributedobject.url"
+  val DistributedObjectName = "test"
+
+  val props = new SystemProperties
+  var lsUrl = if (props contains PropertyKeyLookupServiceUrl) props(PropertyKeyLookupServiceUrl) else "http://localhost:8080"
+  val lookupServiceUri : URI = URI.create(lsUrl)
+  var doUrl = if (props contains PropertyKeyDistributedObjectUrl) props(PropertyKeyDistributedObjectUrl) else "http://localhost:8080"
+  val distributedObjectUri : URI = URI.create(doUrl)
+  val registrationSucceeded = CommunicationSubobject.register(lookupServiceUri, DistributedObjectName, distributedObjectUri)
+  if (registrationSucceeded){
+    DOServer.startServer("0.0.0.0", distributedObjectUri.getPort)
+  }
+  else{
+    println(s"Could not register the distributed object with name $distributedObjectUri on location $distributedObjectUri with the lookup service at $lookupServiceUri")
+  }
 }
