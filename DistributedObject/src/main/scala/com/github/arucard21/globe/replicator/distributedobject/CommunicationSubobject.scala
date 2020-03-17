ae349c4d4156085bb724e92edcee726702004e85
Stefanos Koffas <koffas.stefanos@gmail.com>
2020-03-17 19:30:49 +0100

    communication: Implementation of getlocations message.
    This is not correct (yet).
    
    Signed-off-by: Stefanos Koffas <koffas.stefanos@gmail.com>

--- DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/CommunicationSubobject.scala
+++ DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/CommunicationSubobject.scala
@@ -4,13 +4,17 @@ import java.net.URI
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.model.Uri.Path
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse, StatusCodes, Uri}
 import akka.stream.ActorMaterializer
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Await}
+import java.util.UUID
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import scala.concurrent.duration.Duration
 import scala.util.Try
 
 case class Registration(name: String, location: URI)
@@ -23,9 +27,53 @@ object CommunicationSubobject {
   val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
 
+  def findLocationsForDistributedObject(lookupServiceUri : Uri, distributedObjectName : String) = {
+    val getIdFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+      method = HttpMethods.GET,
+      uri = lookupServiceUri + s"/getId/$distributedObjectName"
+    ))
+    getIdFuture.flatMap ( (getIdResponse) => {
+        if (getIdResponse.status == StatusCodes.OK) {
+          val objectIdString: String = Await.result(Unmarshal(getIdResponse).to[String], Duration.Inf)
+          val objectId = UUID.fromString(objectIdString)
+          val getLocationsFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+            method = HttpMethods.GET,
+            uri = lookupServiceUri + s"/getLocations/${objectId.toString}"
+          ))
+          getLocationsFuture.flatMap( (getLocationsResponse) => {
+              if (getLocationsResponse.status == StatusCodes.OK) {
+                val objectLocationsJson = Await.result(Unmarshal(getLocationsResponse).to[String], Duration.Inf)
+                val objectLocationsList = mapper.readValue[Array[String]](objectLocationsJson)
+                Future.successful(objectLocationsList.map(location => Uri(location)))
+              }
+              else
+                Future.failed(new IllegalStateException(s"""Could not retrieve the locations of the object with the ID "$objectId" on the lookup service at: $lookupServiceUri"""))
+            })
+        }
+        else
+          Future.failed(new IllegalStateException(s"""Could not retrieve the ID of the object with the name "$distributedObjectName" on the lookup service at: $lookupServiceUri"""))
+    })
+  }
+
   def send_request = {
     ???
   }
+
+  //def send_request(requestUri : URI, method : String, param: Int, onCompleteFunction : (Try[HttpResponse]) => Unit) = {
+  //  var endpoint = requestUri + "/" + method
+  //  var request_method = HttpMethods.GET
+
+  //  if (method == "setNumber") {
+  //    endpoint += "/" + param.toString
+  //    request_method = HttpMethods.POST
+  //  }
+
+  //  val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+  //    method = request_method,
+  //    uri = endpoint
+  //  ))
+  //  responseFuture.onComplete(onCompleteFunction)
+  //}
 
   def register(lookupServiceUri : URI, name : String, location : URI, onCompleteFunction : (Try[HttpResponse]) => Unit) = {
     val registration = new Registration(name, location)
--- DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/DOApplication.scala
+++ DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/DOApplication.scala
@@ -6,6 +6,11 @@ import akka.http.scaladsl.model.StatusCodes
 
 import scala.sys.SystemProperties
 import scala.util.{Failure, Success}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration.Duration
+import akka.http.scaladsl.model.Uri
 
 object DOApplication extends App {
   val distributedObjectName = "test"
@@ -39,4 +44,9 @@ object DOApplication extends App {
           println(s"""The registration of the distributed object with name "$distributedObjectName" and location "$distributedObjectUri" failed on the lookup service at: $lookupServiceUri""")
       case Failure(_) => println(s"""The request for the registration of the distributed object with name "$distributedObjectName" and location "$distributedObjectUri" failed on the lookup service at: $lookupServiceUri""")
     })
+
+  val mapper = new ObjectMapper() with ScalaObjectMapper
+  mapper.registerModule(DefaultScalaModule)
+  mapper.writeValueAsString((Await.result(CommunicationSubobject.findLocationsForDistributedObject(URI.create("http://localhost:8080"), "test"), Duration.Inf)))
+
   }
