2d4642cea0e50e734420bca882a814cb888e2989
Ivo Nelissen <ivo@ivo.gg>
2020-03-17 05:46:11 +0100

    Initial implementation of replication subobject

--- DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/ReplicationSubobject.scala
+++ DistributedObject/src/main/scala/com/github/arucard21/globe/replicator/distributedobject/ReplicationSubobject.scala
@@ -1,28 +1,65 @@
 package com.github.arucard21.globe.replicator.distributedobject
 
+sealed trait GlobeMessage { def message: String }
+case object Invoke extends GlobeMessage { val message = "Invoke" }
+case object InvokeSend extends GlobeMessage { val message = "Send" }
+case object Return extends GlobeMessage { val message = "Return" }
+case class Fail(message: String) extends GlobeMessage
+
 object ReplicationSubobject {
-
-  def start = {
-    ???
+  private var lock = false;
+  def start(method: String, parameter: Int = 0): GlobeMessage = {
+    method match {
+      case "setNumber" => setNumber(parameter)
+      case "getNumber" => Invoke // TODO: add checks? not sure when to fail this
+      case _ => Fail("Unknown method called")
+    }
   }
 
-  def invoked = {
-    ???
+  def setNumber(parameter: Int): GlobeMessage = {
+    if(lock) {
+      Fail("Local object currently locked.");
+    }
+    // TODO: Replace with correct call to acquire lock
+    if(CommunicationSubobject.acquire_lock) {
+      InvokeSend
+    }
+    Fail("Failed to acquire lock from other objects.")
   }
 
-  def send = {
-    ???
+  def invoked(): Unit = {
+    lock = true
   }
 
-  def finish = {
-    ???
+  def send(method: String, parameter: Int): GlobeMessage = {
+    // TODO: Replace with proper call to sub-object
+    if(CommunicationSubobject.send_request) {
+      Return
+    }
+    Fail(s"Failed to send($method, $parameter)")
   }
 
-  def acquireLock: Boolean = {
-    ???
+  def finish(): GlobeMessage = {
+    lock = false
+    if(CommunicationSubobject.release_lock) {
+      Return
+    }
+    Fail("Failed to release_lock in communication subobject")
   }
 
-  def releaseLock:Boolean = {
-    ???
+  def acquireLock(): Boolean = {
+    if(!lock) {
+      lock = true;
+      true
+    }
+    false
+  }
+
+  def releaseLock():Boolean = {
+    if(lock) {
+      lock = false
+      true
+    }
+    false
   }
 }
