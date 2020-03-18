debacc47d7096efc12607ec6562e0d915b195069
arucard21 <arucard21@gmail.com>
2020-03-18 16:09:05 +0100

    Add script for running the evaluation automatically

---
+++ evaluateSystem.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# move previous measurements
+if [ -f ./DistributedObject/responseTimesScalability.csv ]
+then
+  mv -v ./DistributedObject/responseTimesScalability.csv ./DistributedObject/responseTimesScalability_prev.csv
+fi
+if [ -f ./DistributedObject/responseTimesConcurrency.csv ]
+then
+  mv -v ./DistributedObject/responseTimesConcurrency.csv ./DistributedObject/responseTimesConcurrency_prev.csv
+fi
+# deploy the system and run the evaluation multiple times
+for replicas in 2 3 5 10
+do
+  ./deploySystem.sh $replicas
+  for iteration in {1..20}
+  do
+    ./gradlew systemTest
+  done
+  ./gradlew terminateEC2Instances
+done
