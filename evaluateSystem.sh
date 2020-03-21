273b1782f6d73fecb4d28b5bcad2cdcd21d6c62f
arucard21 <arucard21@gmail.com>
2020-03-21 10:22:21 +0100

    Wait between system tests and document why we need to wait

--- evaluateSystem.sh
+++ evaluateSystem.sh
@@ -8,15 +8,19 @@ if [ -f ./DistributedObject/responseTimesConcurrency.csv ]
 then
   mv -v ./DistributedObject/responseTimesConcurrency.csv ./DistributedObject/responseTimesConcurrency_prev.csv
 fi
-# deploy the system and run the evaluation multiple times
+# deploy the system with different amounts of Distributed Object replicas
 for replicas in 2 3 5 10
 do
   ./deploySystem.sh $replicas
+  # Give the system some time to ensure it's fully started and ready for testing
   sleep 60
+  # Run the evaluation tests multiple times on this deployment
   for iteration in {1..20}
   do
     ./gradlew systemTest --rerun-tasks
+    # Give the system time to fully finish the system test and become ready for the next iteration of testing
+    sleep 30
   done
-  sleep 30
+  # Shut down the system and remove the EC2 instances
   ./gradlew terminateEC2Instances
 done
