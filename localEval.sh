76b730100f27d3b59ece147f607c1281231c3bda
BigSorry <lexmeulenkamp@hotmail.nl>
2020-03-24 12:38:03 +0100

    correct working dir

--- localEval.sh
+++ localEval.sh
@@ -8,18 +8,17 @@ if [ -f ./DistributedObject/responseTimesConcurrency.csv ]
 then
   mv -v ./DistributedObject/responseTimesConcurrency.csv ./DistributedObject/responseTimesConcurrency_prev.csv
 fi
-# deploy the system with different amounts of Distributed Object replicas
-gnome-terminal --working-directory="/media/lex/Evo/Delft/Distributed/GlobeReplicator" --command "./gradlew :LookupService:run" & 
+workingDir=$PWD
+gnome-terminal --working-directory="$workingDir" --command "./gradlew :LookupService:run" & 
 sleep 5
+# deploy the system with different amounts of Distributed Object replicas
 port=8081
 for i in {1..10}
   do
-  gnome-terminal --working-directory="/media/lex/Evo/Delft/Distributed/GlobeReplicator" --command "./gradlew :DistributedObject:run -Pdistributedobject.url=http://localhost:$port" &
+  gnome-terminal --working-directory="$workingDir" --command "./gradlew :DistributedObject:run -Pdistributedobject.url=http://localhost:$port" &
   ((port++))
   sleep 1
 done
-
-# Give the system some time to ensure it's fully started and ready for testing
 # Run the evaluation tests multiple times on this deployment
 for iteration in {1..20}
 do
