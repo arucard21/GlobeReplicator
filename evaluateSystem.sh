a7037bf3431a9322bac1637e4da9209d3e0b455f
arucard21 <arucard21@gmail.com>
2020-04-01 17:19:46 +0200

    Run experiments from an EC2 instance instead of the local machine to reduce latency.

--- evaluateSystem.sh
+++ evaluateSystem.sh
@@ -9,6 +9,10 @@ if [ -f ./DistributedObject/responseTimesConcurrency.csv ]
 then
   mv -v ./DistributedObject/responseTimesConcurrency.csv ./DistributedObject/responseTimesConcurrency_prev.csv
 fi
+echo "$(date -Iminutes): Creating evaluator EC2 instance"
+# Deploy the EC2 instance used to run the evaluation experiments
+./gradlew deployEvaluator
+mv instanceIds instanceIdEvaluator
 # deploy the system with different amounts of Distributed Object replicas
 for replicas in 2 3 5 10
 do
@@ -16,12 +20,12 @@ do
   ./deploySystem.sh $replicas
   echo "$(date -Iminutes): Finished deploying system with $replicas replicas"
   # Give the system some time to ensure it's fully started and ready for testing
-  sleep 60
+  sleep 30
   # Run the evaluation tests multiple times on this deployment
   for iteration in {1..20}
   do
 	echo "$(date -Iminutes): Running test iteration $iteration on system with $replicas replicas"
-    ./gradlew systemTest --rerun-tasks
+    ssh -o stricthostkeychecking=no ubuntu@$(cat evaluatorHost) "cd GlobeReplicator; ./gradlew -Dorg.gradle.jvmargs='-Xmx256m -Xms256m' -Plookupservice.url=$(cat lookupServiceUrl) systemTest"
     echo "$(date -Iminutes): Finished running test iteration $iteration on system with $replicas replicas"
     # Give the system time to fully finish the system test and become ready for the next iteration of testing
     sleep 5
@@ -30,4 +34,13 @@ do
   echo "$(date -Iminutes): Terminating EC2 instances"
   ./gradlew terminateEC2Instances
 done
+echo "$(date -Iminutes): Copying measurements from evaluator EC2 instance"
+# Copy back the measurements taken in the evaluator EC2 instance
+scp -o stricthostkeychecking=no ubuntu@$(cat evaluatorHost):~/GlobeReplicator/DistributedObject/responseTimesScalability.csv "responseTimesScalability.csv"
+scp -o stricthostkeychecking=no ubuntu@$(cat evaluatorHost):~/GlobeReplicator/DistributedObject/responseTimesConcurrency.csv "responseTimesConcurrency.csv"
+# Clean up the evaluator EC2 instance
+echo "$(date -Iminutes): Terminating evaluator EC2 instance"
+mv instanceIdEvaluator instanceIds
+./gradlew terminateEC2Instances
+rm evaluatorHost
 echo "$(date -Iminutes): Finished evaluation"
